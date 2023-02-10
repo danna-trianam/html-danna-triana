9db6d516a7ce262bd53243b9b2fe74b9fe2d9185
danna-trianam <123768266+danna-trianam@users.noreply.github.com>
2023-02-10 14:02:06 -0500

    cardAdj

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      name: "1",
+      img: "images/1.jpg"
+    }
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+});

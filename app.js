ae5a0d88c91ce2dfbb6be1bd4ec31d44d51d2b3f
danna-trianam <123768266+danna-trianam@users.noreply.github.com>
2023-03-10 13:54:58 -0500

    10/03/23

--- app.js
+++ app.js
@@ -2,63 +2,52 @@ document.addEventListener("DOMContentLoaded", () => {
   const cardAdj = [
     {
       name: "1",
-      img: "images/1.jpg"
-    }
-
+      img: "img/1.jpg"
+    },
     {
       name: "2",
-      img: "images/2.jpg"
-    }
-
+      img: "img/2.jpg"
+    },
     {
       name: "3",
-      img: "images/3.jpg"
-    }
-
+      img: "img/3.jpg"
+    },
     {
       name: "4",
-      img: "images/4.jpg"
-    }
-
+      img: "img/5.jpg"
+    },
     {
       name: "5",
-      img: "images/5.jpg"
-    }
-
+      img: "img/5.jpg"
+    },
     {
       name: "6",
-      img: "images/6.jpg"
+      img: "img/6.jpg"
     }
-
-    {
-      name: "7",
-      img: "images/7.jpg"
-    }
-
-    {
-      name: "8",
-      img: "images/8.jpg"
-    }
-
   ];
-
   const cuadricula = document.querySelector(".cuadricula");
   const resultado = document.querySelector("#resultado");
   var cartasEscogidas = [];
   var cartasEscogidasId = [];
   var cartasGanadas = [];
-});
 
-function crearTablero (){
-  for (let i = 0; i < cardAdj.lenght; i++){
-
-    carta.setAttribute("src","images/reverso.png");
-
-    carta.setAttribute("data-id", i);
-
-    carta.addEventListener("click", voltearCarta);
-
-    cuadricula.appendChild(carta);
-
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+      cuadricula.appendChild(carta);
+    }
   }
-}
+  function voltearCarta() {
+     var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+     if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  crearTablero();
+});

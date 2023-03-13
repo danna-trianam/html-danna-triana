8607c0e2958d74cef4798b3192830457d65b5afb
danna-trianam <123768266+danna-trianam@users.noreply.github.com>
2023-03-13 12:40:52 -0500

    13/03/23

--- app.js
+++ app.js
@@ -23,6 +23,30 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       name: "6",
       img: "img/6.jpg"
+    },
+    {
+      name: "1",
+      img: "img/1.jpg"
+    },
+    {
+      name: "2",
+      img: "img/2.jpg"
+    },
+    {
+      name: "3",
+      img: "img/3.jpg"
+    },
+    {
+      name: "4",
+      img: "img/5.jpg"
+    },
+    {
+      name: "5",
+      img: "img/5.jpg"
+    },
+    {
+      name: "6",
+      img: "img/6.jpg"
     }
   ];
   const cuadricula = document.querySelector(".cuadricula");
@@ -34,18 +58,18 @@ document.addEventListener("DOMContentLoaded", () => {
   function crearTablero() {
     for (let i = 0; i < cardAdj.length; i++) {
       var carta = document.createElement("img");
-      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("src", "img/reverso.png");
       carta.setAttribute("data-id", i);
       carta.addEventListener("click", voltearCarta);
       cuadricula.appendChild(carta);
     }
   }
   function voltearCarta() {
-     var cardId = this.getAttribute("data-id");
+    var cardId = this.getAttribute("data-id");
     cartasEscogidas.push(cardAdj[cardId].name);
     cartasEscogidasId.push(cardId);
     this.setAttribute("src", cardAdj[cardId].img);
-     if (cartasEscogidas.length === 2) {
+    if (cartasEscogidas.length === 2) {
       setTimeout(verificarPareja, 1000);
     }
   }
